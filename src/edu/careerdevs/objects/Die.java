f52e2e2dfc668eb7dd400bdf946da5f7faf18254
Jim Doyle <indellibleink@gmail.com>
2020-09-25 21:04:59 -0400

    creating a die class

--- src/edu/careerdevs/objects/Die.java
+++ src/edu/careerdevs/objects/Die.java
@@ -1,12 +1,16 @@
 package edu.careerdevs.objects;
 
+import java.util.Random;
+
 public class Die {
 
     private int faces;
     private int faceUp;
 
     public Die(int faces){
+        Random rand=new Random();
         this.faces=faces;
-
+        this.faceUp= rand.nextInt(faces)+1;
     }
+
 }
