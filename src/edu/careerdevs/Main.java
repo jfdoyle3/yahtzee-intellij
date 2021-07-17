87ca62a0ee6a6edc4a5efe93f6c3e14cd2043b3a
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2021-07-16 23:59:34 -0400

    trying to update a key on a linkedhashmap

--- src/edu/careerdevs/Main.java
+++ src/edu/careerdevs/Main.java
@@ -13,23 +13,23 @@ public class Main {
         LinkedHashMap<String, Integer> topCard=BasicHashCard.cardTop();
         LinkedHashMap<String, Integer> bottomCard=BasicHashCard.cardBottom();
 
-        String key="Ones";
-        int value=5;
-
-        topCard.put(key,value);
+//        String key="Ones";
+//        int value=5;
 
 
-        System.out.println("--------------------");
+
+
+        System.out.println("-----------------------");
         for (Map.Entry<String,Integer> entry : topCard.entrySet())
-            System.out.printf("| %s     |%d|\n",entry.getKey(), entry.getValue());
+            System.out.printf("| %s     | %d |\n",entry.getKey(), entry.getValue());
 
-        System.out.println("--------------------");
+        System.out.println("-----------------------");
 
-        System.out.println("--------------------");
-        for (Map.Entry<String,Integer> entry : bottomCard.entrySet())
-            System.out.printf("| %s     |%d|\n",entry.getKey(), entry.getValue());
-
-        System.out.println("--------------------");
+//        System.out.println("--------------------");
+//        for (Map.Entry<String,Integer> entry : bottomCard.entrySet())
+//            System.out.printf("| %s     | %d |\n",entry.getKey(), entry.getValue());
+//
+//        System.out.println("--------------------");
 
 
     }
